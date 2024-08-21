ab666d34c3d2ce4895941a2241481971a2fc0838
Amanda Perry <challenge641@gmail.com>
2024-08-20 19:04:03 -0500

    set up conditional to check data.media_type to display video if response happens to be a video

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -1,15 +1,20 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch) // click event calls our function
 
 function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  const url = `https://api.nasa.gov/planetary/apod?api_key=tdXdWueYeMTHnIb14olBzm4OshzIS8doXqO3a4WW&date=${choice}`
+  const choice = document.querySelector('input').value.toLowerCase() // grabs value from input
+  const url = `https://api.nasa.gov/planetary/apod?api_key=tdXdWueYeMTHnIb14olBzm4OshzIS8doXqO3a4WW&date=${choice}` //plugs that value into the url
 
-  fetch(url)
+  fetch(url) // makes the fetch with it
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('img').src = data.hdurl
+        if( data.media_type === 'image') {
+          document.querySelector('img').src = data.hdurl
+        } else if ( data.media_type === 'video') {
+          document.querySelector('iframe').src = data.url
+        }
+        
         document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
