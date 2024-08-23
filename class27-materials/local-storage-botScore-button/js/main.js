cda416562ae4a8b1eae9f258984021bc5dd68b80
Amanda Perry <challenge641@gmail.com>
2024-08-23 12:54:40 -0500

    changes to api

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,21 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+if (!localStorage.getItem('botScore')){
+  localStorage.setItem('botScore', 0)
+}
+
+
+
+document.querySelector('button').addEventListener('click', addAnothaOne) // click event calls our function
+
+function addAnothaOne(){
+  // get item and store it
+  // manipulate it
+  // set it in local storage
+  let botScoreVal = Number(localStorage.getItem('botScore'))
+  botScoreVal += 1
+  localStorage.setItem('botScore', botScoreVal)
+
+}
+
+
 
 
